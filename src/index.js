04ac2017687b0780b3118b981ae67876a6aadd49
Nickyi175 <tome.andrea1378@gmail.com>
2024-05-27 13:38:52 -0600

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-// src/index.js
-import express from 'express';
-
+const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
-
