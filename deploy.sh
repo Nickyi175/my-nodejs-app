2b1b23f0274ee1ec3894c033f6a5294413d0aa7d
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2024-05-27 00:12:09 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Deploying application..."
-# Aquí irían los comandos reales para desplegar la aplicación, como copiar archivos a un servidor
+# Agrega aquí los comandos necesarios para desplegar tu aplicación
