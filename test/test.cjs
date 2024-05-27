a4a2e7ea0ddabb7a3be7621575ebe9750de3faf7
Nickyi175 <tome.andrea1378@gmail.com>
2024-05-27 01:02:03 -0600

    PROBANDO

---
+++ test/test.cjs
@@ -0,0 +1,10 @@
+require = require('esm')(module /*, options*/);
+
+import { expect } from 'chai';
+
+describe('My Test Suite', function() {
+  it('should pass', function() {
+    expect(true).to.be.true;
+  });
+});
+
