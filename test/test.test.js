f042ad011ca5c0b403c43f8e9a9e881417c4a42e
Nickyi175 <tome.andrea1378@gmail.com>
2024-06-01 20:57:18 -0600

    test

--- test/test.test.js
+++ test/test.test.js
@@ -1,17 +1,7 @@
 const request = require('supertest');
-const app = require('../src/index');
+const app = require('../src/index'); // Ajusta la ruta según sea necesario
 
 describe('GET /', () => {
-    let server;
-
-    beforeAll(() => {
-        server = app.listen(4000);
-    });
-
-    afterAll((done) => {
-        server.close(done);
-    });
-
     it('should return 200 OK', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
